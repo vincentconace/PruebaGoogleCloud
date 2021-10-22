502df5a59d3ce5330af98b7d9a6ca628ed24d615
extviconace <ext_viconace@mercadolibre.com>
2021-10-22 14:20:37 -0300

    Template succesful

--- quickstart.go
+++ quickstart.go
@@ -1,20 +1,3 @@
-/**
- * @license
- * Copyright Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START drive_quickstart]
 package main
 
 import (
@@ -89,7 +72,7 @@ func saveToken(path string, token *oauth2.Token) {
 
 func main() {
 	ctx := context.Background()
-	b, err := ioutil.ReadFile("client_secret_4431693249-ii6e25v04cbafghjat57vj1grmao6t9s.apps.googleusercontent.com.json")
+	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
@@ -120,5 +103,3 @@ func main() {
 		}
 	}
 }
-
-// [END drive_quickstart]
